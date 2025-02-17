da8b07d105cf2b70f9efea31b54dc75c24d1063f
SmartW0rker <hristijanchochevski@gmail.com>
2025-02-17 21:34:49 +0100

    disabling csrf

--- src/main/java/com/kiko/config/SecurityConfig.java
+++ src/main/java/com/kiko/config/SecurityConfig.java
@@ -7,17 +7,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.sql.DataSource;
-import javax.xml.crypto.Data;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -30,8 +23,9 @@ public class SecurityConfig {
         /*http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());*/
         /*http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());*/
 
-        http
+        http.sessionManagement(smc -> smc.invalidSessionUrl("/invalidSession").maximumSessions(1).maxSessionsPreventsLogin(true))
 //                .requiresChannel(rcc -> rcc.anyRequest().requiresSecure()) only HTTPS
+                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
                 .authorizeHttpRequests(requests ->
                 requests.requestMatchers("/account","/balance","/cards","/loans","/user").authenticated()
                         .requestMatchers("/notices","contact","/error","/register").permitAll());
